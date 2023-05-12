2fe0eea702446fc0e47d1d6d1ca1698ca5183eae
Shahealam Kasar <kasarshahealam@gmail.com>
2023-05-12 21:45:58 +0530

    Second Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Shahealam Saniya");
+            System.Console.WriteLine("Hello CSharp Language");
         }
     }
 }
